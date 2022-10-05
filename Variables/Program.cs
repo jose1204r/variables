e7f18f039c547f827691bef123bc08abaa633786
jose <jrodriguez@itsolutionspcllc.com>
2022-10-04 23:46:19 -0400

    Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿string myCusing = ("Michael");
+int age = 26;
+bool cusingAge = true;
+double currentYear = 2022;
+decimal mycusinPount = 130.5m;
+
+Console.WriteLine($"My cusin {myCusing} has {age} and {mycusinPount} pount so far in tis year of {currentYear} ");
+
+Console.WriteLine($"");
+
+if (cusingAge == true)
+
+{
+
+    Console.WriteLine($" Yes {age} years old");
+}
+
